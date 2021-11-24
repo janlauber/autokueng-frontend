00872665ac89c5a29edb64bfaf9a4aa740094638
Jan <jan.lauber@protonmail.ch>
2021-11-24 01:23:24 +0100

    removed links
    Signed-off-by: Jan <jan.lauber@protonmail.ch>

--- components/HeroCard.js
+++ components/HeroCard.js
@@ -22,13 +22,13 @@ function HeroCard({ data }) {
                   <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
                     <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
                       <a
-                        href="#"
+                        href=""
                         className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-blue-700 bg-white hover:bg-blue-50 sm:px-8"
                       >
                         Öffnungszeiten
                       </a>
                       <a
-                        href="#"
+                        href=""
                         className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8"
                       >
                         Kontakt
