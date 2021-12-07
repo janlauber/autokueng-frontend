dcda1c599f50fe2ae0d6954fddb9fd2bffda9696
Jan Lauber <jan.lauber@protonmail.ch>
2021-12-07 21:27:53 +0100

    adding fahrzeugpark autoscout24
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- components/services/Services.js
+++ components/services/Services.js
@@ -3,10 +3,11 @@ import Image from 'next/image'
 function Services({ services }) {
     return (
         <>
-        <div className="pt-10">
-            <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-4xl lg:text-5xl">
-                Services
-            </h1>
+        <div className="">
+            <div className="my-6 text-center">
+				<h1 className="my-2 font-bold text-4xl">Services</h1>
+				<h2 className="text-xl text-blue-600 font-semibold">Unsere Dienstleistungen im Überblick</h2>
+			</div>
             <div className="grid sm:grid-cols-3 gap-4 ">
 
             {services.map((service) => (
@@ -15,7 +16,7 @@ function Services({ services }) {
                     <div className="">
                         <img src="https://via.placeholder.com/600/92c952" />
                     </div>
-                    <h2 className="font-bold text-xl">{service.title}</h2>
+                    <h2 className="font-bold text-4xl">{service.title}</h2>
                     <p className="text-gray-800 italic text-sm">{service.body}</p>
                 </div>
             ))}
--- pages/fahrzeugpark.js
+++ pages/fahrzeugpark.js
@@ -1,23 +1,19 @@
-
+import Script from 'next/script'
+import Head from 'next/head'
 
 export default function Fahrzeugpark() {
     return (
-        <div className="portfolio-contant">
-			<div id="autoscout24">
-				<h2>Autoscout24</h2>
-				<div data-embedded-src="https://www.autoscout24.ch/de/hci/list?design=846&filter=1276" className="embedded-content-area"></div>
-				<script src="https://www.autoscout24.ch/MVC/Content/as24-hci-desktop/js/e.min.js"></script>
+		<div className="text-center">
+			<div className="my-6">
+				<h1 className="my-2 font-bold text-4xl">Fahrzeugpark</h1>
+				<h2 className="text-xl text-blue-600 font-semibold">Autoscout24</h2>
 			</div>
-						{/* <div id="autolina">
-							<h2>Autolina</h2>
-							<script src="https://www.autolina.ch/iframe/iframe.js" type="text/javascript"></script>
-							<iframe style="width:1px;min-width:100%;" data-header-height="100" src="https://www.autolina.ch/iframe/2214?footer=none" onload="init_autolina_iframe()" frameborder="0" allowfullscreen scrolling="no" id="autolina-car-details-iframe"></iframe>
-						</div>
-						<div id="carforyou">
-							<h2>CarForYou</h2>
-							<div data-prop-cfy-dealer-id="1239" data-prop-cfy-locale="de" id="cfy-listing-widget"></div><script async src="https://widget.carforyou.ch/widget.bundle.js" type="text/javascript"></script>
-            </div> */}
-          </div>
+			<div data-embedded-src="https://www.autoscout24.ch/de/hci/list?design=846&filter=1276" className="embedded-content-area"></div>
+			<Head>
+				<script async src="https://www.autoscout24.ch/MVC/Content/as24-hci-desktop/js/e.min.js"/>
+			</Head>
+		</div>
     )
   }
 
+  
