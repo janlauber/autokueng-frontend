4bcf7650dc463019d9b3cdb1bdeaac5ed56d3018
Jan Lauber <jan.lauber@protonmail.ch>
2021-12-09 00:42:35 +0100

    change News get with useState
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- components/landingpage/News.js
+++ components/landingpage/News.js
@@ -1,6 +1,15 @@
 import Image from 'next/image'
 
 function News({ data }) {
+  var title = ""
+  var content = ""
+  var picture = ""
+  data.map((news => {
+    title = news.title
+    content = news.content
+    picture = news.picture
+  }))
+
 
     return (
       <div className="py-3 pt-10 sm:max-w-xl sm:mx-auto">
@@ -10,14 +19,14 @@ function News({ data }) {
         <div className="px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <div className="max-w-md mx-auto">
             <div>
-              <img src={data.picture} className="h-7 sm:h-8" />
+              <img src={picture} className="h-7 sm:h-8" />
             </div>
             <div className="divide-y divide-gray-200">
               <div className="py-8  leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                 <h1 className="text-center text-2xl font-extrabold tracking-tight sm:text-3xl lg:text-4xl">
-                    <span className="block text-blue-500">{data.title}</span>
+                    <span className="block text-blue-500">{title}</span>
                 </h1>
-                <p>{data.content}</p>
+                <p>{content}</p>
               </div>
             </div>
           </div>
--- pages/index.js
+++ pages/index.js
@@ -5,28 +5,50 @@ import Services from '../components/landingpage/Services'
 import Stats from '../components/landingpage/Stats'
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
+import { Fragment, useEffect, useState } from 'react'
 
-export async function getServerSideProps() {
-  const res = await fetch('http://localhost:8000/api/v1/news', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-  const news = await res.json()
+// export async function getServerSideProps() {
+//   const res = await fetch('http://localhost:8000/api/v1/news', {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   })
+//   const news = await res.json()
 
-  if(!news) {
-    throw new Error('No news found')
-  }
-  return {
-    props: {
-      news,
-    },
-  }
-}
+//   if(!news) {
+//     throw new Error('No news found')
+//   }
+//   return {
+//     props: {
+//       news,
+//     },
+//   }
+// }
 
-export default function Home({news}) {
+export default function Home() {
 
+  const [news, setNews] = useState([])
+  useEffect(() => {
+    (
+      async () => {
+        try {
+          const res = await fetch('http://localhost:8000/api/v1/news', {
+            credentials: 'include',
+            }) 
+          const json = await res.json()
+          
+          if(!json) {
+            throw new Error('No news found')
+          }
+
+          setNews(json)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    )()
+  }, [])
   return (
     <>
       <Navbar />
