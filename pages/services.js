4bac4612f5cc1a373d9b6622b8620fac026104b1
Jan Lauber <jan.lauber@protonmail.ch>
2021-12-12 19:19:52 +0100

    remove sample api data
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- pages/services.js
+++ pages/services.js
@@ -2,31 +2,13 @@ import Services from "../components/services/Services"
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
 
-// get API Data
-export async function getStaticProps() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-  const services = await res.json()
-  if(!services) {
-    return {
-      notFound: true,
-    }
-  }
-  return {
-    props: {
-      services,
-    },
-
-    revalidate: 10,
-  }
-}
-
-export default function Fahrzeugpark({services}) {
+export default function Fahrzeugpark(props) {
     return (
       <>
         <Head>
           <title>Autokueng - Services</title>
         </Head>
-        <Services services={services} />
+        <Services auth={props.auth} />
       </>
     )
   }
